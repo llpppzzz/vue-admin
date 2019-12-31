eeb451393636b2e853ab9edb14d8e01a1022961d
lipeizhou <llpppzzz@163.com>
2019-12-31 10:52:00 +0800

    测试环境富文本不加载问题

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const API_REQUEST_CONTENT_TYPE = 'application/json' // API Content-Type é
 export const TABLE_DEFAULT_PAGE_SIZE = 10
 export const UEDITOR_CONFIG = {
   serverUrl: '',
-  UEDITOR_HOME_URL: '/UEditor/',
+  UEDITOR_HOME_URL: process.env.NODE_ENV === 'production' ? '/test/AttractAdmin/UEditor/' : '/UEditor/',
   aotuHeightEnabled: false,
   autoFloatEnabled: false,
   initialFrameHeight: 320
