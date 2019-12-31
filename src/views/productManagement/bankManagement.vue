6c1a631cb4d8e009927b04b17adb36df83774abe
lipeizhou <llpppzzz@163.com>
2019-12-31 14:56:00 +0800

    添加银行页面

---
+++ src/views/productManagement/bankManagement.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="view-bank-management">
+    <div class="searching-box">
+      <l-date-picker v-model="dateRange" @change="reloadList"></l-date-picker>
+      <el-button size="small" type="primary" plain @click="openDetails">新建银行</el-button>
+    </div>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="图片" align="center">
+        <template slot-scope="scope">
+          <span v-null="scope.row.name">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="银行名称" align="center">
+        <template slot-scope="scope">
+          <span v-null="scope.row.name">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否上架" align="center">
+        <template slot-scope="scope">
+          <span v-null="scope.row.name">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="light" content="编辑" placement="top-start">
+            <el-button size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="light" content="删除" placement="top-start">
+            <el-button size="mini" type="danger" icon="el-icon-delete" circle></el-button>
+          </el-tooltip>
+          <el-button size="mini" type="success" plain @click="openDetails(scope.row)">上架</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BankManagement',
+  data() {
+    return {
+      dateRange: [],
+      listLoading: false,
+      list: [{}]
+    }
+  },
+  methods: {
+    reloadList(val) {
+      console.log(this.dateRange)
+    },
+    onSearch() {},
+    openDetails(row) {
+      this.$router.push({ path: '/productManagement/newBank' })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .view-bank-management {
+    background: #fff;
+    .searching-box {
+      padding: 0 0 16px 0;
+      display: flex;
+      justify-content: space-between;
+      .el-input {
+        width: 200px;
+      }
+    }
+  }
+</style>
---
+++ src/views/productManagement/newBank.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="view-new-bank">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="银行图标">
+        <el-upload
+          class="upload-demo"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :file-list="fileList"
+          list-type="picture">
+          <el-button plain size="small" type="primary">点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="银行名称">
+        <el-input v-model="form.bankName" />
+      </el-form-item>
+      <el-form-item label="是否上架">
+        <el-radio v-model="form.display" label="1">是</el-radio>
+        <el-radio v-model="form.display" label="2">否</el-radio>
+      </el-form-item>
+      <el-form-item label="地区选择">
+        <el-cascader
+          placeholder="选择或搜索地区"
+          :options="options"
+          filterable>
+        </el-cascader>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" plain @click="onSubmit">新建</el-button>
+        <el-button type="info" plain @click="onCancel">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewBank',
+  data() {
+    return {
+      fileList: [{
+        name: 'food.jpeg',
+        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+      }],
+      options: [],
+      form: {
+        prodType: '1',
+        bankName: '',
+        display: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+    },
+    onCancel() {
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview(file) {
+      console.log(file)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .view-new-bank{
+  }
+</style>
